fc51c8440a44dc491c330453c9df1ac9d98d2c0c
“rkumari2” <rubinakumari@mail.com>
2023-09-11 21:25:25 +0100

    done notFound page

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css'
 
 export default function NotFoundPage() {
   return (
-    <>
-    <h1>Not found page</h1>
-    </>
+    <div id='notFoundPage'>
+    <img id='not-found' src="paper.png" alt="" />
+
+    <h1 id='not-found-text'>404: Page Not Found</h1>
+    <button id='home-btn'><Link to='/'> Home </Link></button>
+    </div>
   )
 }
