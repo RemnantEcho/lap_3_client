bd1dc1d53b581eba0ad4bda059c9f2b385364765
“rkumari2” <rubinakumari@mail.com>
2023-09-12 10:53:20 +0100

    sign up page skeleton

--- src/pages/SignupPage/index.jsx
+++ src/pages/SignupPage/index.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 export default function SignupPage() {
   return (
-    <>
-    <h1>sign up page</h1>
-    </>
+    <div id="form-container">
+      <h2>Sign Up</h2>
+      <form id="form">
+        <input type="text" id="Name" name="Name" placeholder="First Name" />
+        <input type="text" id="Surname" name="Surname" placeholder="Last Name" />
+        <input type="text" id="username" name="username" placeholder="Username" />
+        <input type="text" id="Email" name="Email" placeholder="Email" />
+        <input type="password" id="password" name="password" placeholder="Password" />
+        <input type="submit" value="Register" id="register-btn" />
+      </form>
+      <div id="login">
+        <p>Already Registered? <a id="link-login" href="./login.html">Log In here</a></p>
+      </div>
+    </div>
   )
 }
