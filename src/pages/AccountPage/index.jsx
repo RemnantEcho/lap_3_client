2a540c0c17589113ed0057721e6d368f6c44b67a
“rkumari2” <rubinakumari@mail.com>
2023-09-12 11:02:04 +0100

    accountPage skeleton

--- src/pages/AccountPage/index.jsx
+++ src/pages/AccountPage/index.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 export default function AccountPage() {
   return (
-    <>
-    <h1>account page</h1>
-    </>
+    <div className='account-page' style={{display:'flex', flexDirection:'column'}}>
+    <h3> Name: </h3>
+    <p> Fetched name </p>
+    <h3> Surname: </h3>
+    <p> Fetched surname </p>
+    <h3> username: </h3>
+    <p> Fetched username </p>
+    <h3> email: </h3>
+    <p> Fetched email </p>
+    <button>Edit details</button>
+    <h3> Change password </h3>
+    <form>
+      <input type="text" name="old password" id="old password" placeholder='Old password' />
+      <input type="text" name="New password" id="New password" placeholder='New password' />
+      <button>Change</button>
+    </form>
+    </div>
   )
 }
