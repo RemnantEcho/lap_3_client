bb0f06ca61e4b19cca6ea971dba3ca9e836e61ed
hasanshahid99 <shahidhasan148@gmail.com>
2023-09-13 13:07:00 +0100

    LoginForm and authentication occuring. Need to adjust the endpoints and CORS on backend to allow for post requests from our front-end

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Provider} from 'react-redux'
-
+import { AuthProvider } from './context/AuthProvider';
 import store from './store'
 
 import App from './App'
@@ -10,10 +10,12 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <AuthProvider>
         <Provider store={store}>
             <Router>
                 <App />
             </Router>
         </Provider>
+        </AuthProvider>
     </React.StrictMode>
 );
