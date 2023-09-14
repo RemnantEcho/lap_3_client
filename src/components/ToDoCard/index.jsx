b5f64544fe60366631d24896af04167352fdf9f7
“rkumari2” <rubinakumari@mail.com>
2023-09-14 12:09:28 +0100

    Complete button working

--- src/components/ToDoCard/index.jsx
+++ src/components/ToDoCard/index.jsx
@@ -3,62 +3,64 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
 const ToDoCard = () => {
-    const [results, setResults] = useState([])
+  const [results, setResults] = useState([])
 
-    const { date } = useParams()
+  useEffect(() => {
 
-    useEffect(() => {
-
-        // if (!date) {
-          const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:3000/goals')
-                if (response.status === 200) {
-                    const responseData = response.data
-                    const data = responseData.goal
-                    if (Array.isArray(data)) {
-                        setResults(data)
-                    } else {
-                        console.error('Data is not an array')
-                    }
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error)
-            }
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/goals')
+        if (response.status === 200) {
+          const responseData = response.data
+          const data = responseData.goal
+          if (Array.isArray(data)) {
+            setResults(data)
+          } else {
+            console.error('Data is not an array')
+          }
         }
-        fetchData()
-        // } else {
-        //   const fetchDatabyDate = async () => {
-        //     try {
-        //         const response = await axios.get(`http://localhost:3000/goals/${date}`)
-        //         if (response.status === 200) {
-        //             const responseData = response.data
-        //             const data = responseData.goal
-        //             if (Array.isArray(data)) {
-        //                 setResults(data)
-        //             } else {
-        //                 console.error('Data is not an array')
-        //             }
-        //         }
-        //     } catch (error) {
-        //         console.error('Error fetching data:', error)
-        //     }
-        // }
-        // }  // I have modified the code to check whether a parameter is provided using an if statement, and show the output according to that. 
-        
-      }, [])
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    fetchData()
 
+  }, [])
+
+
+  const handleComplete = async (_id) => {
+    try {
+      const response = await axios.patch(`http://localhost:3000/goals/${_id}`, {
+        progressValue: 'Complete',
+      });
+  
+      if (response.status === 200) {
+        setResults((prevState) =>
+          prevState.map((item) =>
+            item._id === _id ? { ...item, progressValue: 'Complete' } : item
+          )
+        );
+      }
+    } catch (error) {
+      console.error('Error updating data:', error);
+    }
+  };
 
   return (
     <div>
       <ul>
         {results.map((item) => (
-          <li style={{listStyle: 'none'}}key={item._id}>
-            <div>
+          <li style={{ listStyle: 'none', border:'1px black solid', backgroundColor:'wheat' }} key={item._id}>
+            <div id='content-cont'>
               <h3>{item.goal}</h3>
               <p> Date: {item.date} </p>
               <p>Category: {item.category}</p>
               <p>Progress: {item.progressValue}</p>
+            </div>
+            <div id='actions-cont'>
+              <button>Delete</button>
+              <button>Edit </button>
+              <button onClick={() => handleComplete(item._id)}>Complete</button>
             </div>
           </li>
         ))}
